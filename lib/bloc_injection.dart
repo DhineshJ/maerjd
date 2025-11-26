ec9666c4d297e9ef7e2331f0677eb3ec854cfe13
Dhinesh <dhineshrider087@gmail.com>
2025-11-26 23:33:54 +0530

    adding for merge

--- lib/bloc_injection.dart
+++ lib/bloc_injection.dart
@@ -4,6 +4,7 @@ import 'package:notes/bloc/notes_bloc.dart';
 final bl = GetIt.instance;
 final cl= GetIt.instancessssssssssse;
 final dl= GetIt.instancesssssssssss;
+final dl= GetIt.instancesssssssssssf for merge ;
 
 
 void registerBloc() {
