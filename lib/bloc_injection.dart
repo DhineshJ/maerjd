3a1b8cca94a5406627e115ede91f3c7b34eb4fe6
Dhinesh <dhineshrider087@gmail.com>
2025-11-26 23:16:20 +0530

    adding cl

--- lib/bloc_injection.dart
+++ lib/bloc_injection.dart
@@ -2,6 +2,8 @@ import 'package:get_it/get_it.dart';
 import 'package:notes/bloc/notes_bloc.dart';
 
 final bl = GetIt.instance;
+final cl= GetIt.instance;
+
 
 void registerBloc() {
   bl.registerLazySingleton<NotesBloc>(
