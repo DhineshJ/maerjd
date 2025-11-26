ded223682d1acaa5f11a2da7c3681216d19d0d24
Dhinesh <dhineshrider087@gmail.com>
2025-11-26 23:23:02 +0530

    adding ss in main

--- lib/bloc_injection.dart
+++ lib/bloc_injection.dart
@@ -2,8 +2,8 @@ import 'package:get_it/get_it.dart';
 import 'package:notes/bloc/notes_bloc.dart';
 
 final bl = GetIt.instance;
-final cl= GetIt.instance;
-final dl= GetIt.instance;
+final cl= GetIt.instancessssssssssse;
+final dl= GetIt.instancesssssssssss;
 
 
 void registerBloc() {
